862e7f062ef82c0586274a619d5f74c627912f3c
Zhansultan <barlibay@bimes.kz>
2021-01-14 20:59:25 +0600

    second

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let a = 3;
+let b = 4;
+let a2 = Math.pow(a, 2);
+let b2 = Math.pow(b, 2);
+let c = Math.sqrt(a2 + b2);
+document.write(c);
+
+
+
+/*
+let name = "Marker";
+let color = "red";
+let code = 8802;
+*/
+
+/*
+console.log(name);
+console.log(color);
+console.log(code);
+document.write(name);
+document.write("<h1>" + name + "</h1>");
+DOM
+document.write(10 + 20 + 30 * 2);
+document.write(25 % 10);
+
+document.write(Math.pow(4, 2));
+
+*/
+
+
+/*
+
++ - сложение
+- - вычитание
+* - умножение
+/ - деление
+% - получени остатка от деления
+
+Math.pow(x, y)	- x^y	- возведени в степень
+Math.pow(16)	- 4		- квадратный корень
+
+*/
+
+
+/*
+с доски:
+"book" > String
+"128" > Integer
+"3.1415" > Double
+
+"true", "false" > Booleab
+*/
+
+// home work: сайт SOLOLEARN выучить JS до скриптов!
