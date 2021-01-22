af795c180cce61f33f432c6209dd8ed9d7f991df
Zhansultan Chuakbayev <barlibaybro@gmail.com>
2021-01-22 19:01:40 +0600

    third

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
+function getValue() {
+	const first = document.getElementById("first");
+	const second = document.getElementById("second");
+
+//	const result = document.getElementById("result");
+
+	result.innerHTML = parseInt(first.value) + parseInt(second.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let a = 3;
 let b = 4;
 let a2 = Math.pow(a, 2);
 let b2 = Math.pow(b, 2);
 let c = Math.sqrt(a2 + b2);
 document.write(c);
-
-
+*/
 
 /*
 let name = "Marker";
@@ -22,33 +42,50 @@ document.write("<h1>" + name + "</h1>");
 DOM
 document.write(10 + 20 + 30 * 2);
 document.write(25 % 10);
-
 document.write(Math.pow(4, 2));
-
 */
 
-
 /*
-
 + - сложение
 - - вычитание
 * - умножение
 / - деление
 % - получени остатка от деления
-
 Math.pow(x, y)	- x^y	- возведени в степень
 Math.pow(16)	- 4		- квадратный корень
-
 */
 
-
 /*
-с доски:
+типы данных:
 "book" > String
-"128" > Integer
-"3.1415" > Double
-
-"true", "false" > Booleab
+"128" > Integer / number
+"3.1415" > Double / Float
+true / false > boolean
 */
 
 // home work: сайт SOLOLEARN выучить JS до скриптов!
+
+/*
+Переменные
+var
+let
+const (не меняется)
+
+increment:
+a++
+a+=1
+a=a+1
+
+discrement:
+a--
+a-=1
+a=a-1
+*/
+
+/*
+let a = 10;
+if (a % 10 == 0) {
+	a+=1000;
+}
+	console.log (a)
+*/
